cb74f7e6ccc3cd9cafd06a813723381d340d99ab
Besufekad mekuria <tobesufekad@gmail.com>
2012-02-25 18:09:29 +0300

    hello this is mine 1

---
+++ MODERNETH/src/firstproject/moderneth/MODERNETHActivity.java
@@ -0,0 +1,13 @@
+package firstproject.moderneth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MODERNETHActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
